3d1176b6209695e4d77918460b0c4bef2d3114b6
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-07-06 17:02:00 -0600

    Historia 1-3 Se agrega el template de vista que nos servirá como base para más adelante. Se muestran los tamaños de las pizzas disponibles así como unas pocos coberturas.

--- Pizzeria/Pizzeria/wwwroot/js/site.js
+++ Pizzeria/Pizzeria/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
+﻿function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
