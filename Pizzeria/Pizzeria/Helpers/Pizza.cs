896048e0424892a9121e84c042ba954d1ed3be4b
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-07-07 08:46:52 -0600

    Historia 4 Se agregan las posibles coberturas y sus precios. Además se crea la lógica del método que se encargará se establecer todas las coberturas de la pizza

--- Pizzeria/Pizzeria/Helpers/Pizza.cs
+++ Pizzeria/Pizzeria/Helpers/Pizza.cs
@@ -12,9 +12,19 @@ namespace Pizzeria.Helpers
     }
     public class Pizza
     {
+        private const int PrecioHongos = 500;
+        private const int PrecioCebolla = 500;
+        private const int PrecioChile = 500;
+        private const int PrecioSalami = 1000;
+        private const int PrecioJamon = 1000;
+        private const int PrecioPepperoni = 1000;
+        private const int PrecioPiña = 1500;
+        private const int PrecioQuesoExtra = 1500;
+
+        private const int PrecioEnvio = 2000;
+
         public string Tamaño { get; set; }
         public List<Cobertura> Coberturas { get; set; }
-
 
         public void EstablecerTamaño(string tamaño)
         {
@@ -25,7 +35,42 @@ namespace Pizzeria.Helpers
         {
             Cobertura tipoCobertura = new Cobertura { Tipo = cobertura, Precio = precio };
 
-            Coberturas.Add(tipoCobertura);
+            if (tipoCobertura != null)
+                Coberturas.Add(tipoCobertura);
+        }
+
+        public void EstablecerCoberturas(string[] coberturas) {
+            int precio = -1;
+            foreach (var tipo in coberturas) {
+                switch (tipo) {
+                    case "hongos":
+                        precio = PrecioHongos;
+                        break;
+                    case "cebolla":
+                        precio = PrecioCebolla;
+                        break;
+                    case "chile":
+                        precio = PrecioChile;
+                        break;
+                    case "salami":
+                        precio = PrecioSalami;
+                        break;
+                    case "jamon":
+                        precio = PrecioJamon;
+                        break;
+                    case "pepperoni":
+                        precio = PrecioPepperoni;
+                        break;
+                    case "piña":
+                        precio = PrecioPiña;
+                        break;
+                    case "queso":
+                        precio = PrecioQuesoExtra;
+                        break;
+                }
+
+                AgregarCobertura(tipo, precio);
+            }
         }
     }
 }
