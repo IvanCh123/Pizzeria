37906e8d5dd615ef153f1169a6303b0bf0c0fd54
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-07-07 08:49:46 -0600

    Proyecto de test unitarios Se agrega el proyecto base para los test unitarios de cada funcionalidad de la pizzeria

---
+++ Pizzeria/PizzeriaTester/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PizzeriaTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
