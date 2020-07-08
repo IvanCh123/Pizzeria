ed5085abd93fad4074470394cea606acff014ea8
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-07-07 18:41:07 -0600

    Cambios en la vista Se cambia el template antiguo del navbar por uno nuevo y se agrega su css y js respectivamente. Se elimina la imagen antigua que se tenía en el html

--- Pizzeria/Pizzeria/wwwroot/js/site.js
+++ Pizzeria/Pizzeria/wwwroot/js/site.js
@@ -1,8 +1,55 @@
-﻿function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus glyphicon-minus');
+﻿"use strict";
+
+// grabbing the class names from the data attributes
+const navBar = $('.navbar'),
+    data = navBar.data(); // booleans used to tame the scroll event listening a little..
+
+let scrolling = false,
+    scrolledPast = false; // transition Into
+
+function switchInto() {
+    // update `scrolledPast` bool
+    scrolledPast = true; // add/remove CSS classes
+
+    navBar.removeClass(data.startcolor);
+    navBar.removeClass(data.startsize);
+    navBar.addClass(data.intocolor);
+    navBar.addClass(data.intosize);
+    console.log('into transition triggered!');
 }
-$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-$('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+; // transition Start
+
+function switchStart() {
+    // update `scrolledPast` bool
+    scrolledPast = false; // add/remove CSS classes
+
+    navBar.addClass(data.startcolor);
+    navBar.addClass(data.startsize);
+    navBar.removeClass(data.intocolor);
+    navBar.removeClass(data.intosize);
+    console.log('start transition triggered!');
+} // set `scrolling` to true when user scrolls
+
+
+$(window).scroll(() => scrolling = true);
+setInterval(() => {
+    // when `scrolling` becomes true... 
+    if (scrolling) {
+        // set it back to false
+        scrolling = false; // check scroll position
+
+        if ($(window).scrollTop() > 100) {
+            // user has scrolled > 100px from top since last check
+            if (!scrolledPast) {
+                switchInto();
+            }
+        } else {
+            // user has scrolled back <= 100px from top since last check
+            if (scrolledPast) {
+                switchStart();
+            }
+        }
+    } // take a breath.. hold event listener from firing for 100ms
+
+}, 100);
