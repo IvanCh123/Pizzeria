877f5850650a2ced8b0102a95114f7df282cc3a2
Ivan Chavarría <ivanjavier2000@gmail.com>
2020-07-08 18:24:43 -0600

    Historias 6-8 (Finalizadas) Se agrega en la nueva vista de FinalizarOrden un resumen de la compra por realizar (desglose). A su vez se agregan los botones 'Cancelar' y 'Finalizar' con su retroalimentacion respectiva.

--- Pizzeria/Pizzeria/Pages/Index.cshtml.cs
+++ Pizzeria/Pizzeria/Pages/Index.cshtml.cs
@@ -8,6 +8,10 @@ namespace Pizzeria.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Resultado { get; set; }
+        public void OnGet(string retroalimentacion) {
+            Resultado = retroalimentacion;
+        }
         public void OnPost()
         {
             Response.Redirect("/Ordenar/Ordenar");
--- Pizzeria/Pizzeria/Pages/Ordenar/FinalizarOrden.cshtml.cs
+++ Pizzeria/Pizzeria/Pages/Ordenar/FinalizarOrden.cshtml.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Pizzeria.Pages.Ordenar
 {
     public class FinalizarOrdenModel : PageModel
     {
         public string MontoFinal { get; set; }
-        public void OnGet(string PrecioOrden)
+        public void OnGet(string MontoActual, string PrecioOrden)
         {
             MontoFinal = PrecioOrden;
+        }
+
+        public IActionResult OnPostFinalizar()
+        {
+            string direccion = Request.Form["direccion"];
+
+            return RedirectToPage("../Index", new { retroalimentacion = "Su orden se ha registrado y será enviada a: "+ direccion });
+        }
+
+        public IActionResult OnPostCancelar() {
+            return RedirectToPage("../Index", new {retroalimentacion = "Su orden ha sido cancelada con éxito"});
         }
     }
 }
